2b55f0ad97d0cefc2b5119643976c787cfb6e824
Your Name <loveme6230@naver.com>
2020-10-07 23:24:25 +0900

    일정관리 컴포넌트 준비

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -3,14 +3,16 @@ import React, { Component } from "react";
 // import "./App.css";
 // sass활용
 // import SassComponent from "./SassComponent";
-import CSSModule from "./CSSModule";
+// import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponents";
 
 class App extends Component {
   render() {
     return (
       <div>
         {/* <SassComponent /> */}
-        <CSSModule />
+        {/* <CSSModule /> */}
+        <StyledComponent />
       </div>
     );
   }
--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,11 +1,18 @@
 import React from "react";
-import styles from "./CSSModule.module.css";
+// import styles from "./CSSModule.module.css";
+import styles from "./CSSModule.module.scss";
+// classnames 라이브러리
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles); // 미리 styles에서 클래스를 받아 오도록 설정하고
 
 const CSSModule = () => {
   return (
     // <div className={`${styles.wrapper} ${styles.inverted}`}>
     // 템플릿 리터럴 문법 활용
-    <div className={[styles.wrapper, styles.inverted].join("")}>
+    // <div className={[styles.wrapper, styles.inverted].join("")}>
+    // classnames에 내장된 bind함수 사용
+    <div className={cx("wrapper", "inverted")}>
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
   );
